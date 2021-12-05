c2d39badbd321e7f8265484dc63acf9e9b34de92
Jesse Sanchez <jesse@henosishospitality.com>
2021-12-05 06:50:45 -0500

    added nav and images

---
+++ client/src/components/Navigation/Navigation.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../Navigation/Navigation.scss";
+
+export default function Navigation() {
+  return (
+    <a href="https://www.jbluery.com/">
+      <nav className="NavBar"></nav>
+    </a>
+  );
+}
