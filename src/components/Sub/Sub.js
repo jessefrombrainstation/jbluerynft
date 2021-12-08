895fb578a1ddfc05ba8dea96cf9e5096c629c83c
Jesse Sanchez <jesse@henosishospitality.com>
2021-12-08 13:59:17 -0500

    updated email function

--- src/components/Sub/Sub.js
+++ src/components/Sub/Sub.js
@@ -5,12 +5,16 @@ import coin from "../../assets/images/coin-transparent.gif";
 export default function Sub() {
   return (
     <div>
-      <form className="sub">
+      <form
+        className="sub"
+        action="https://formspree.io/f/xyyoyzrj"
+        method="POST"
+      >
         <label>
           <input
             className="sub__email"
             type="email"
-            name="email"
+            name="_replyto"
             placeholder="email"
           />
         </label>
