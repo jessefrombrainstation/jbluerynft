adf988db416f5b1905244f47272b96756e8c6d33
Jesse Sanchez <jesse@henosishospitality.com>
2021-12-29 09:38:12 -0500

    rearranged icon properties and renamed to global icon

--- src/components/ConnectBar/ConnectBar.js
+++ src/components/ConnectBar/ConnectBar.js
@@ -11,21 +11,21 @@ function ConnectBar() {
         <a href="https://discord.gg/WvuxmvUT">
           <img
             src={discord}
-            className="connect-container__discord"
+            className="connect-container__icon"
             alt="join-our-club"
           />
         </a>
         <a href="https://www.instagram.com/jbluery/?hl=en">
           <img
             src={instagram}
-            className="connect-container__instagram"
+            className="connect-container__icon"
             alt="follow-on-the-gram"
           />
         </a>
         <a href="https://twitter.com/jbluery">
           <img
             src={twitter}
-            className="connect-container__twitter"
+            className="connect-container__icon"
             alt="trending-on-twitter"
           />
         </a>
