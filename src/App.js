bc47a8e2cf29390c463aaa2f7e1bffc81304f9d4
Jesse Sanchez <jesse@henosishospitality.com>
2021-12-30 06:21:24 -0500

    extended website, added flightmap, added mission control, added responsiveness to new features and adapted, added team info, ready for deeper integrations after receving NFTS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Navigation from "./components/Navigation/Navigation";
-import spaceground from "./assets/images/spaceground-with-footer.png";
+import spacegroundTop from "./assets/images/spaceground-with-footer-top.png";
+import spacegroundBttm from "./assets/images/spaceground-with-footer-bottom.png";
 import "./App.scss";
 import Hero from "./components/Hero/Hero";
 import Ether from "./components/Eth/Eth";
@@ -9,6 +10,11 @@ import Nft from "./components/Nft/Nft";
 import Sub from "./components/Sub/Sub";
 import Original from "../src/assets/images/jbluery-ufo-og.png";
 import NFTs from "../src/assets/images/pink-sauce-nft-first-edition.png";
+import flightmap from "../src/assets/images/flightmap.png";
+import missioncontrol from "../src/assets/images/mission-control.png";
+import guimonitor from "../src/assets/images/interface-v2-unreleased.gif";
+import team from "../src/assets/images/team.png";
+import teamportrait from "../src/assets/images/team-portrait.png";
 import ConnectBar from "./components/ConnectBar/ConnectBar";
 import "../src/components/Board/Board.scss";
 
@@ -46,8 +52,8 @@ function App() {
               <div className="og-container">
                 <p className="og-info">
                   <img src={Original} className="og" alt="og" />
-                  Our captain "pink" and his flying saucer welcomes you to our
-                  members-only club.
+                  Our captain "pink" and his flying saucer "sauce" welcomes you
+                  to our members-only club.
                   <br />
                   <br />
                   Being a member unlocks first dibs on new pink sauce NFT
@@ -55,12 +61,16 @@ function App() {
                 </p>
               </div>
               <p className="nft-info">
-                With only 911 planned release this is a very exclusive club.
+                With only 911 planned releases this is a very exclusive club.
                 Most of pink and his flying saucer attributes are inspired by
                 technology advancements, space crafts, race cars, art culture
                 and cities. <br />
                 <br />
                 Stay tuned for our first edition launch!
+                <br />
+                Just launched! *10/17 2021*
+                <br />
+                SOLD OUT
               </p>
               <img
                 src={NFTs}
@@ -70,12 +80,92 @@ function App() {
                   setShow(true);
                 }}
               />
+              <p className="nft-flightmap">
+                <img
+                  src={flightmap}
+                  className="flightmap"
+                  alt="flightmap-pink-sauce-nft-2022"
+                />
+                <p className="nft-flightmap__goals1">
+                  Abduct our cousins and make them join the team willingly
+                </p>
+                <p className="nft-flightmap__goals2">
+                  Promote pink sauce NFT on a big mural in the heart of miami
+                  during art basel week
+                </p>
+                <p className="nft-flightmap__goals3">
+                  Release first edition *10/17 2021*
+                </p>
+                <p className="nft-flightmap__goals4">
+                  Open space bar lounge on discord
+                </p>
+                <p className="nft-flightmap__goals5">
+                  Member exclusive teaser video for next launch
+                </p>
+                <p className="nft-flightmap__goals6">drop merch</p>
+                <p className="nft-flightmap__goals7">
+                  Release second pack *hidden date*
+                </p>
+                <p className="nft-flightmap__goals8">
+                  Release third pack *hidden date*
+                </p>
+                <p className="nft-flightmap__goals9">
+                  Release fourth pack *hidden date*
+                </p>
+                <p className="nft-flightmap__goals10">
+                  Release arcade game playable with only verified pink sauce NFT
+                  tokens
+                </p>
+                <p className="nft-flightmap__goals11">
+                  Open space bar lounge in wynwood miami florida
+                </p>
+              </p>
+              <p className="nft-missioncontrol">
+                <img
+                  src={missioncontrol}
+                  className="missioncontrol"
+                  alt="mission-control-pink-sauce-nft"
+                />
+                <img
+                  src={guimonitor}
+                  className="monitor"
+                  alt="mission-control-pink-sauce-nft-status"
+                />
+                <p className="nft-missioncontrol__status">
+                  It appears the signal is not strong enough yet. please check
+                  back later! <br /> Follow our social media pages to stay
+                  updated.
+                </p>
+              </p>
+              <p className="nft-team">
+                <img src={team} className="team" alt="team-pink-sauce-nft" />
+                <p className="nft-team__introduction">
+                  Pink Sauce is one of jbluery's ART pieces local to Miami,
+                  Florida. This iconic character can be found in mutiple murals
+                  throughout wynwood "the art capital of the nation".
+                  <br />
+                  <br />
+                  The team is ready to launch its fleet of 911 pink sauce NFTs
+                  into space!
+                  <br />
+                  <img
+                    src={teamportrait}
+                    className="team-portrait"
+                    alt="team-portrait-pink-sauce-nft"
+                  />
+                </p>
+              </p>
             </div>
             <div className="App">
               <header className="spaceground"></header>
               <img
-                src={spaceground}
-                className="spaceground"
+                src={spacegroundTop}
+                className="spaceground__top"
+                alt="spaceground"
+              />
+              <img
+                src={spacegroundBttm}
+                className="spaceground__bottom"
                 alt="spaceground"
               />
             </div>
